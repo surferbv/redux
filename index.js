aad716111752d5a3662a008f713267704fb6010f
Brent Varga <surferbv@gmail.com>
2022-08-21 20:06:49 -0700

    Some notes (#1)

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ function createStore () {
  // 2. Get the state
  // 3. Listen to changes on the state
  // 4. Update the state
- let state;
+ let state
+ let listeners = [] // allows us to call subscribe many times an array of functions
 
- const getState = () => state;
+ const getState = () => state
+
+ const subscribe = (listeners) => {
+  listeners.push(listeners)
+
+  // to unsubscribe by returning a brand-new function from subscribe
+  // which will remove our listeners function from our listeners array
+  return () => {
+   listeners = listeners.filter((l) => l !== listeners)
+  }
+ }
 
  return {
-  getState
+  getState,
+  subscribe
  }
 }
+
+// How to use the store
+// // to creates a store
+// const store = createStore()
+//
+// // to subscribes
+// store.subscribe( () => {})
+//
+// // to unsubscribe
+// const unsubscribe = store.subscribe(()=>{})
