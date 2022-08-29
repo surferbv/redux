d30103568bb12eb4938eb693ef58985758522718
surferbv <surferbv@gmail.com>
2022-08-28 18:17:09 -0700

    Add todo our first reducer

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-function createStore () {
- // The store should have four parts
- // 1. The state
- // 2. Get the state
- // 3. Listen to changes on the state
- // 4. Update the state
 
- let state
- let listeners = []
-
- const getState = () => state
-
- const subscribe = (listeners) => {
-  listeners.push(listeners)
-
-  return () => {
-   listeners = listeners.filter((l) => l !== listeners)
-  }
- }
-
- return {
-  getState,
-  subscribe
- }
-}
-
-// Can't just allow anyone to make change to the state
-// Similar to an American FootBall Team
-// The team needs to operate as one cohesive unit to increase predictable.
-// They do this by creating a playbook were each individual on the team will know this playbook by heart.
-// Just like a team will have a playbook i.e. a collection of actions which represents the various state that can change the store.
-
-// Example of various actions which can change the state of our store.
+/*
 const add_todo = {
  type: 'ADD_TODO',
  todo: {
@@ -55,6 +24,45 @@ const add_goal = {
 
 const remove_goal = {
  id: 0
+}
+*/
+
+/*
+* Todo reducer
+* The default state is [] if one does not exist.
+* Instead of pushing the state we concat it because push would mutate the state.
+* */
+function todos(state = [], action) {
+ if (action.type === 'ADD_TODO'){
+  return state.concat([action.todo])
+ }
+ return state
+}
+
+function createStore () {
+ // The store should have four parts
+ // 1. The state
+ // 2. Get the state
+ // 3. Listen to changes on the state
+ // 4. Update the state
+
+ let state
+ let listeners = []
+
+ const getState = () => state
+
+ const subscribe = (listeners) => {
+  listeners.push(listeners)
+
+  return () => {
+   listeners = listeners.filter((l) => l !== listeners)
+  }
+ }
+
+ return {
+  getState,
+  subscribe
+ }
 }
 
 
