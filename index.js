3a3df10c286b5b3c9b4b928cb67f218d855c5be6
surferbv <surferbv@gmail.com>
2022-08-28 17:47:15 -0700

    Updating the state

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ function createStore () {
  // 2. Get the state
  // 3. Listen to changes on the state
  // 4. Update the state
+
  let state
- let listeners = [] // allows us to call subscribe many times an array of functions
+ let listeners = []
 
  const getState = () => state
 
  const subscribe = (listeners) => {
   listeners.push(listeners)
 
-  // to unsubscribe by returning a brand-new function from subscribe
-  // which will remove our listeners function from our listeners array
   return () => {
    listeners = listeners.filter((l) => l !== listeners)
   }
@@ -25,12 +24,45 @@ function createStore () {
  }
 }
 
-// How to use the store
-// // to creates a store
-// const store = createStore()
-//
-// // to subscribes
-// store.subscribe( () => {})
-//
-// // to unsubscribe
-// const unsubscribe = store.subscribe(()=>{})
+// Can't just allow anyone to make change to the state
+// Similar to an American FootBall Team
+// The team needs to operate as one cohesive unit to increase predictable.
+// They do this by creating a playbook were each individual on the team will know this playbook by heart.
+// Just like a team will have a playbook i.e. a collection of actions which represents the various state that can change the store.
+
+// Example of various actions which can change the state of our store.
+const add_todo = {
+ type: 'ADD_TODO',
+ todo: {
+  id: 0,
+  name: 'Learn Redux',
+  complete: false,
+ }
+}
+
+const remove_todo = {
+ type: 'REMOVE_TODO',
+ id: 0,
+}
+
+const add_goal = {
+ type: 'ADD_GOAL',
+ goal: {
+  id: 0,
+  name: 'Run a marathon',
+ }
+}
+
+const remove_goal = {
+ id: 0
+}
+
+
+
+
+
+
+
+
+
+
